4f42f43c750fb54b27b002b901493699cbac23c6
Satyam Kumar Singh <82038243+satyams00@users.noreply.github.com>
2022-12-05 23:54:17 +0530

    notes First Commit

---
+++ mysql_connect.php
@@ -0,0 +1,26 @@
+<?php
+    $servername = "127.0.0.1";
+    $username = "root";
+    $password = "";
+    $dbname = "notes_crud";
+
+    $conn = new mysqli($servername, $username, $password);
+    $sql_createdb = "CREATE DATABASE IF NOT EXISTS $dbname";
+    $sql_createtb = "CREATE TABLE IF NOT EXISTS notes (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        title VARCHAR(100),
+        content VARCHAR(200),
+        priority VARCHAR(15)
+        )";
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+
+    $conn->query($sql_createdb);
+    $conn->select_db($dbname);
+    $conn->query($sql_createtb);
+
+    return $conn;
+?>
